de1824b69b353ebba3643efdba2d29160a092c2d
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2023-12-25 15:33:30 +0200

    add 2 task

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,5 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import izitoast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const myInput = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
@@ -17,34 +19,35 @@ const options = {
   onClose(selectedDates) {
     const selectedData = selectedDates[0];
     const initialTimer = selectedData.getTime() - new Date().getTime();
-    const RenderTimer = convertMs(initialTimer);
+    const renderTimer = convertMs(initialTimer);
 
     if (selectedDates[0] < new Date()) {
+      iziToast.error({
+        position: 'topRight',
+        message: 'Please choose a date in the future',
+      });
       startBtn.setAttribute('disabled', true);
-      alert('Please choose a date in the future');
     } else {
       startBtn.removeAttribute('disabled');
       userSelectedDate = selectedData;
-    }
 
-    timerDay.textContent = `${RenderTimer.days}`;
-    timerHours.textContent = `${RenderTimer.hours}`;
-    timerMinutes.textContent = `${RenderTimer.minutes}`;
-    timerSeconds.textContent = `${RenderTimer.seconds}`;
+      timerDay.textContent = `${renderTimer.days}`;
+      timerHours.textContent = `${renderTimer.hours}`;
+      timerMinutes.textContent = `${renderTimer.minutes}`;
+      timerSeconds.textContent = `${renderTimer.seconds}`;
+    }
   },
 };
 const fp = flatpickr(myInput, options);
 let userSelectedDate = fp;
 
 startBtn.addEventListener('click', () => {
-  const currentTime = new Date().getTime();
   const selectedTime = userSelectedDate.getTime();
 
   const timerInterval = setInterval(() => {
+    const currentTime = new Date().getTime();
     let remainingTime = selectedTime - currentTime;
     const numberOfTimer = convertMs(remainingTime);
-    console.log(remainingTime);
-    console.log(numberOfTimer);
 
     if (remainingTime <= 0) {
       clearInterval(timerInterval);
@@ -65,13 +68,21 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = Math.floor(ms / day)
+    .toString()
+    .padStart(2, '0');
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = Math.floor((ms % day) / hour)
+    .toString()
+    .padStart(2, '0');
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = Math.floor(((ms % day) % hour) / minute)
+    .toString()
+    .padStart(2, '0');
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second)
+    .toString()
+    .padStart(2, '0');
 
   return { days, hours, minutes, seconds };
 }
